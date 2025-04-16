c1567383b9a3d828df7e3d11353409c7a4b25aaf
Subhansini <109660922+Subhansini@users.noreply.github.com>
2025-04-16 23:22:34 +0530

    Update index.jsx

--- 01. Static pages/10. Why React? It's Declarative!/index.jsx
+++ 01. Static pages/10. Why React? It's Declarative!/index.jsx
@@ -19,3 +19,7 @@ Don't use innerHTML to accomplish any of this.
     
 */
 
+const h1 = document.createElement(h1)
+h1.textComntent = "Imperative"
+h1.className = "header"
+document.getElementById("root").appeandChild(h1)
